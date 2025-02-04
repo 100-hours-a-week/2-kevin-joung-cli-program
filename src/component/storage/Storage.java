bde262ffbc3e277f20657bfd47ff7a8bbb9d4da4
HyunWoo Jeong <jrwedo@naver.com>
2025-02-04 13:09:37 +0900

    Refactor: 구현이 필요없는 Storage를 추상클래스로 변경

--- src/component/storage/Storage.java
+++ src/component/storage/Storage.java
@@ -2,16 +2,11 @@ package component.storage;
 
 import component.Component;
 
-public class Storage extends Component {
+public abstract class Storage extends Component {
     int memory;
 
     public Storage(String name, int price, int powerConsumption, String brand, int memory) {
         super(name, price, powerConsumption, brand);
         this.memory = memory;
-    }
-
-    @Override
-    public String getInfo() {
-        return "";
     }
 }
